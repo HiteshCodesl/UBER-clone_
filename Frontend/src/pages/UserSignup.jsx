cc7ffab88ef7cb3dd2418b63b2f8567f40429a86
Hitesh Navare <navarehitesh65@gmail.com>
2025-02-16 14:42:55 +0530

    updated userSignup with axios

--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -1,24 +1,37 @@
+import axios from "axios"
+import { useState, useContext} from "react"
+import { Link, useNavigate } from "react-router-dom"
+import {UserDataContext} from "../context/UserContext"
 
-import { useState } from "react"
-import { Link } from "react-router-dom"
 function UserSignup() {
        const [email, setEmail] = useState('')
        const [password, setPassword] = useState('')
        const [firstname, setFirstname] = useState('')
        const [lastname, setLastname] = useState('')
-       const [userData, setUserData] = useState({})
+       const [userData, setUserData] = useState({});
 
-       const submitHandler = (e) =>{
+       const navigate = useNavigate();
+
+       const [user, setUser] = useContext(UserDataContext)
+
+       const submitHandler = async(e) =>{
           e.preventDefault();
-          setUserData({
-            fullName: {
+          const newUser = {
+            fullname: {
               firstname: firstname,
               lastname: lastname
-             },
+            },
             email: email,
-            password: password, 
-          })
-            console.log(userData)
+            password: password
+          }
+          const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser)
+
+          if(response.status === 201){
+            const data = response.data
+            setUser(data.user)
+            navigate("/home")
+          }
+
           setFirstname('');
           setEmail('');
           setLastname('');
